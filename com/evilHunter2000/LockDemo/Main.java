cc5ca20fe9e86ee3cc5dfecfe7790822d09c5724
Shayak.mitra <shayakmitra6901@gmail.com>
2025-04-05 16:31:59 +0530

    Added by shayak for the multithreading(lambda expression)

--- com/evilHunter2000/LockDemo/Main.java
+++ com/evilHunter2000/LockDemo/Main.java
@@ -3,12 +3,7 @@ package com.evilHunter2000.LockDemo;
 public class Main {
     public static void main(String[] args) {
         BankAccount sbi = new BankAccount();
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                sbi.withdraw(50);
-            }
-        };
+        Runnable r = () -> sbi.withdraw(50);
         Thread t1=new Thread(r,"thread1");
         Thread t2=new Thread(r,"thread2");
         t1.start();
